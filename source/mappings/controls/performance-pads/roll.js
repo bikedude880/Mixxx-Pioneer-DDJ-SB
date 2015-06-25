6339dc2badeb7bf09821712fa6e585dc55a155c9
Daniel <c720@chromebook>
2015-06-25 16:42:40 -0700

    Moar updates lol

--- source/mappings/controls/performance-pads/roll.js
+++ source/mappings/controls/performance-pads/roll.js
@@ -1,12 +1,12 @@
 var manager = require('./../../../../control-manager.js');
 
-for (var channelIndex = 0; channelIndex < 4; channelIndex++)
+for (var channelIndex = 0; channelIndex < 2; channelIndex++)
 {
 	var group = 'Channel' + (channelIndex + 1);
 
-	for (var padIndex = 0; padIndex < 8; padIndex++)
+	for (var padIndex = 0; padIndex < 4; padIndex++)
 	{
-		manager.add(group, 'PioneerDDJSR.RollPerformancePad', 0x97 + channelIndex, 0x10 + padIndex);
+		manager.add(group, 'PioneerDDJSB.RollPerformancePad', 0x9p + channelIndex, 0x50 + padIndex);
 	}
 }
 
