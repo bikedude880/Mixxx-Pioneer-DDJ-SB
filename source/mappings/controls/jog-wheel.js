fb12199535bbad9deeaa4bb1c02bdaf1ef253de7
Daniel <c720@chromebook>
2015-06-25 16:51:11 -0700

    Whoopsee

--- source/mappings/controls/jog-wheel.js
+++ source/mappings/controls/jog-wheel.js
@@ -5,11 +5,11 @@ for (var channelIndex = 0; channelIndex < 2; channelIndex++)
 {
 	var group = 'Channel' + (channelIndex + 1);
 
-	manager.add(group, 'PioneerDDJSB.jogScratchTouch', 0x9n + channelIndex, 0x36);
-	manager.add(group, 'PioneerDDJSB.jogSeekTouch',	0x9n + channelIndex, 0x35);
-	manager.add(group, 'PioneerDDJSB.jogScratchTurn', 0xBn + channelIndex, 0x22);
-	manager.add(group, 'PioneerDDJSB.jogPitchBend', 0xBn + channelIndex, 0x21);
-	manager.add(group, 'PioneerDDJSB.jogSeekTurn', 0xBn + channelIndex, 0x23);
+	manager.add(group, 'PioneerDDJSB.jogScratchTouch', 0x90 + channelIndex, 0x36);
+	manager.add(group, 'PioneerDDJSB.jogSeekTouch',	0x90 + channelIndex, 0x35);
+	manager.add(group, 'PioneerDDJSB.jogScratchTurn', 0xB0 + channelIndex, 0x22);
+	manager.add(group, 'PioneerDDJSB.jogPitchBend', 0xB0 + channelIndex, 0x21);
+	manager.add(group, 'PioneerDDJSB.jogSeekTurn', 0xB0 + channelIndex, 0x23);
 }
 			 
 module.exports = manager.controls;
