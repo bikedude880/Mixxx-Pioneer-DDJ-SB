58fcf0a7c6225fa00a0ea1f828bdd4caa1859c35
Daniel <c720@chromebook>
2015-06-25 16:39:57 -0700

    Simple updates

--- source/mappings/controls/jog-wheel.js
+++ source/mappings/controls/jog-wheel.js
@@ -1,15 +1,15 @@
 var manager = require('./../../../control-manager.js');
 
-// Generate mappings for four channels.
-for (var channelIndex = 0; channelIndex < 4; channelIndex++)
+// Generate mappings for two channels.
+for (var channelIndex = 0; channelIndex < 2; channelIndex++)
 {
 	var group = 'Channel' + (channelIndex + 1);
 
-	manager.add(group, 'PioneerDDJSR.jogScratchTouch', 0x90 + channelIndex, 0x36);
-	manager.add(group, 'PioneerDDJSR.jogSeekTouch',	0x90 + channelIndex, 0x35);
-	manager.add(group, 'PioneerDDJSR.jogScratchTurn', 0xB0 + channelIndex, 0x22);
-	manager.add(group, 'PioneerDDJSR.jogPitchBend', 0xB0 + channelIndex, 0x21);
-	manager.add(group, 'PioneerDDJSR.jogSeekTurn', 0xB0 + channelIndex, 0x23);
+	manager.add(group, 'PioneerDDJSB.jogScratchTouch', 0x9n + channelIndex, 0x36);
+	manager.add(group, 'PioneerDDJSB.jogSeekTouch',	0x9n + channelIndex, 0x35);
+	manager.add(group, 'PioneerDDJSB.jogScratchTurn', 0xBn + channelIndex, 0x22);
+	manager.add(group, 'PioneerDDJSB.jogPitchBend', 0xBn + channelIndex, 0x21);
+	manager.add(group, 'PioneerDDJSB.jogSeekTurn', 0xBn + channelIndex, 0x23);
 }
 			 
 module.exports = manager.controls;
