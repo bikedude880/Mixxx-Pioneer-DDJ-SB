f514a6d5f68f2c6bb55d4cc53e6f8d22386c2e65
Daniel <c720@chromebook>
2015-06-26 16:28:10 -0700

    Wipe it all, we're doing this from scratch

--- source/mappings/controls/mixer.js
+++
@@ -1,7 +0,0 @@
-var manager = require('./../../../control-manager.js');
-
-manager.add('Master', 'crossfader', 0xB6, 0x1F, 'soft-takeover');
-manager.add('Master', 'headMix', 0xB6, 0x05, 'soft-takeover');
-
-
-module.exports = manager.controls;
--- source/mappings/controls/playlist.js
+++
@@ -1,6 +0,0 @@
-var manager = require('./../../../control-manager.js');
-
-manager.add('Playlist', 'PioneerDDJSB.RotarySelectorClick', 0x96, 0x41);
-manager.add('Playlist', 'PioneerDDJSB.RotarySelector', 0xB6, 0x40);
-
-module.exports = manager.controls;
