0fc7e17982e014ae244c9b7c15e3fac3512a41a0
Daniel <c720@chromebook>
2015-06-25 20:40:47 -0700

    Whoops, should build and then add/push

--- bin/PIONEER_DDJ-SB-scripts.js
+++ bin/PIONEER_DDJ-SB-scripts.js
@@ -349,7 +349,14 @@ PioneerDDJSB.RotarySelector = function(channel, control, value, status)
 	switch(PioneerDDJSB.status.rotarySelector.target)
 	{
 		case tracklist:
-			engine.setValue('[Playlist]', 'SelectTrackKnob', delta/8);
+			if (value >= 0x01 && value <= 0x1e) {
+				value = value;
+			} else if (value >= 0x62 && value <= 0x7f) {
+				value = 0 - (0x7f-value+1);
+			} else {
+				return;
+			}
+			engine.setValue('[Playlist]', 'SelectTrackKnob', value);
 			break;
 		case libraries:
 			if (delta > 0)
