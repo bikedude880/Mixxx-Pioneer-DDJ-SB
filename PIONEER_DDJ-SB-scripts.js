a4a4e9c27548ad3a521a3893f6c5e85ed7231fa3
Daniel <c720@chromebook>
2015-06-26 18:28:32 -0700

    Take some script from other DDJ-SB github repo

--- PIONEER_DDJ-SB-scripts.js
+++ PIONEER_DDJ-SB-scripts.js
@@ -3,8 +3,6 @@ function PioneerDDJSB() {}
 PioneerDDJSB.init = function(id) {}
 PioneerDDJSB.shutdown = function(id) {}
 
-PioneerDDJSB.offset = 0x40
-
 // Handles the track selection rotation, lifted from CDJ-850
 PioneerDDJSB.RotarySelector = function(channel, control, value, status)
 {
@@ -18,8 +16,37 @@ PioneerDDJSB.RotarySelector = function(channel, control, value, status)
 	engine.setValue('[Playlist]','SelectTrackKnob',value);
 }
 
+// Work out the jog-wheel change / delta
+PioneerDDJSB.getJogWheelDelta = function(value)
+{
+	// The Wheel control centers on 0x40; find out how much it's moved by.
+	return value - 0x40;
+}
+
+// Pitch bend a channel
+PioneerDDJSB.pitchBend = function(channel, movement)
+{
+	var deck = channel + 1;
+	var group = '[Channel' + deck +']';
+
+	// Make this a little less sensitive.
+	movement = movement / 5;
+
+	// Limit movement to the range of -3 to 3.
+	movement = movement > 3 ? 3 : movement;
+	movement = movement < -3 ? -3 : movement;
+
+	engine.setValue(group, 'jog', movement);
+}
+
 PioneerDDJSB.JogWheelSide = function(channel, control, value, status, group)
 {
-	var delta = (value - PioneerDDJB.offset)
-	engine.setValue(group,'jog',delta)
+	var deck = channel + 1;
+	var group = '[Channel' + deck +']';
+
+	// Only pitch-bend when actually playing
+	if (engine.getValue(group, 'play'))
+	{
+		PioneerDDJSB.pitchBend(channel, PioneerDDJSB.getJogWheelDelta(value));
+	}
 }
